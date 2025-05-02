e76ef8f494d5290a7c66bc7721dc4562cb7497bb
Rocky Li <aperocky@gmail.com>
2025-05-01 21:22:44 -0400

    update formula to satisfy brew style

--- tascli.rb
+++ tascli.rb
@@ -1,8 +1,7 @@
 class Tascli < Formula
-  desc "A simple, fast, local CLI tool for tracking tasks and records from unix terminal."
+  desc "Simple, fast, local CLI tool for tracking tasks and records from unix terminal"
   homepage "https://github.com/Aperocky/tascli"
-  version "0.6.0"
-  url "https://github.com/Aperocky/tascli/archive/refs/tags/v#{version}.tar.gz"
+  url "https://github.com/Aperocky/tascli/archive/refs/tags/v0.6.0.tar.gz"
   sha256 "d7e3942d203599caceb12ee5ab808567ad8cc4e03986500b681da8c85c47cf01"
   license "MIT"
 
@@ -13,6 +12,6 @@ class Tascli < Formula
   end
 
   test do
-    assert_match "tascli #{version}", shell_output("#{bin}/tascli --version")
+    assert_match "tascli 0.6.0", shell_output("#{bin}/tascli --version")
   end
 end
